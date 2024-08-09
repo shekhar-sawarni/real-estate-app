56cdcce6d0a1a11ed98031f89206e4919c4531b7
shekhar-sawarni <shekharsawarni@gmail.com>
2024-08-09 18:07:05 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+
+const userSchema =new mongoose.Schema({
+    username:{
+        type: String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true, 
+    },
+    password:{
+        type: String,
+        required:true,
+    }
+},  {timestamps:true});
+
+const User =mon.model('User',userSchema);
+
+export default User;
