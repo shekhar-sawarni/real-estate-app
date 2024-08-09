294f2e161fae7918df97dfd5577a56a889da692a
shekhar-sawarni <shekharsawarni@gmail.com>
2024-08-09 17:18:43 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes path="/" element={<Home/>}></Routes>
-      <Routes path="/sign-in" element={<SignIn />}></Routes>
-      <Routes path="/sign-up" element={<SignUp />}></Routes>
-      <Routes path="/about" element={<About />}></Routes>
-      <Routes path='/profile' element={<Profile/>}></Routes>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
